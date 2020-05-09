240321817af14b39418f606838ac151ac508c479
tgq14 <15201514761@163.com>
2020-05-09 16:34:02 -0600

    update

--- Plato_downstn.sh
+++ Plato_downstn.sh
@@ -4,7 +4,7 @@
 # See: https://www.acrc.bris.ac.uk/protected/bc4-docs/scheduler/index.html
 
 #SBATCH --job-name=downtostn
-#SBATCH --time=2-23:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --mem-per-cpu=20G
 
 #! add the python module
