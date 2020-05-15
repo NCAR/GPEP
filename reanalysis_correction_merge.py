bd894e8d24b7f7b9bd19c82b01979827dd8b6a4e
tgq14 <15201514761@163.com>
2020-05-14 19:32:10 -0600

    update

--- reanalysis_correction_merge.py
+++ reanalysis_correction_merge.py
@@ -466,7 +466,7 @@ file_readownstn = ['/datastore/GLOBALWATER/CommonData/EMDNA/ERA5_day_ds/ERA5_dow
 # output files
 # train and test index file
 # ttindexfile = '/Users/localuser/Research/Test/2layer_train_test_index.npz'
-ttindexfile = '/home/gut428/ReanalysisCorrMerge/path_reastn_cv/2layer_train_test_index.npz'
+ttindexfile = '/home/gut428/ReanalysisCorrMerge/CrossValidate_2layer/2layer_train_test_index.npz'
 
 # near stations
 # near_stnfile = '/Users/localuser/Research/Test/near_stn.npz'
