be1bc305f92791d33116cbddf994c332b5eb5a6e
tgq14 <15201514761@163.com>
2020-05-23 12:16:33 -0600

    modify rea downscale

---
+++ evaluation.py
@@ -0,0 +1,51 @@
+# evaluate downscaled/corrected/merged/regression estimates
+
+import numpy as np
+from scipy import io
+import auxiliary as au
+import os
+
+filestn = '/home/gut428/stndata_whole.npz'
+datatemp = np.load(filestn)
+prcp_stn = datatemp['prcp_stn']
+tmean_stn = datatemp['tmean_stn']
+trange_stn = datatemp['trange_stn']
+stn_ID=datatemp['stn_ID']
+stn_lle=datatemp['stn_lle']
+del datatemp
+nstn, ntimes = np.shape(prcp_stn)
+
+# 1. evaluate downscaled prcp
+file_readown = ['/datastore/GLOBALWATER/CommonData/EMDNA/ERA5_day_ds/ERA5_downto_stn_nearest.npz',
+                '/datastore/GLOBALWATER/CommonData/EMDNA/MERRA2_day_ds/MERRA2_downto_stn_nearest.npz',
+                '/datastore/GLOBALWATER/CommonData/EMDNA/JRA55_day_ds/JRA55_downto_stn_nearest.npz',
+                '/datastore/GLOBALWATER/CommonData/EMDNA/ERA5_day_ds/ERA5_downto_stn_linear.npz',
+                '/datastore/GLOBALWATER/CommonData/EMDNA/MERRA2_day_ds/MERRA2_downto_stn_linear.npz',
+                '/datastore/GLOBALWATER/CommonData/EMDNA/JRA55_day_ds/JRA55_downto_stn_linear.npz',
+                '/datastore/GLOBALWATER/CommonData/EMDNA/ERA5_day_ds/ERA5_downto_stn.npz',
+                '/datastore/GLOBALWATER/CommonData/EMDNA/MERRA2_day_ds/MERRA2_downto_stn.npz',
+                '/datastore/GLOBALWATER/CommonData/EMDNA/JRA55_day_ds/JRA55_downto_stn.npz']
+fileout = 'evaluation_readown.npz'
+fileout2 = 'evaluation_readown.mat'
+if not os.path.isfile(fileout):
+    print('Evaluate downscaled reanalysis')
+    fnum = len(file_readown)
+    met_prcp = np.zeros([fnum, nstn, 4])
+    met_tmean = np.zeros([fnum, nstn, 4])
+    met_trange = np.zeros([fnum, nstn, 4])
+    for i in range(fnum):
+        print('File', i, fnum)
+        datatemp = np.load(file_readown[i])
+        prcp_readown = datatemp['prcp_readown']
+        tmean_readown = datatemp['tmean_readown']
+        trange_readown = datatemp['trange_readown']
+        for g in range(nstn):
+            met_prcp[i, g, :] = au.metric(prcp_stn[g, :], prcp_readown[g, :])
+            met_tmean[i, g, :] = au.metric(tmean_stn[g, :], tmean_readown[g, :])
+            met_trange[i, g, :] = au.metric(trange_stn[g, :], trange_readown[g, :])
+        del datatemp
+
+    np.savez_compressed(fileout,met_prcp=met_prcp,met_tmean=met_tmean,met_trange=met_trange,
+                        stn_ID=stn_ID,stn_lle=stn_lle)
+    io.savemat(fileout2,{'met_prcp':met_prcp, 'met_tmean':met_tmean, 'met_trange':met_trange,
+                         'stn_ID':stn_ID, 'stn_lle':stn_lle})
