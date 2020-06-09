3e35656fd8f6cb30680bbee1a913f30752546e81
tgq14 <15201514761@163.com>
2020-06-09 14:11:57 -0600

    update

--- Plato_corrmerge2.sh
+++ Plato_corrmerge2.sh
@@ -11,7 +11,7 @@
 module load python/3.7.4
 
 # run the application
-srun python -u tempruh.py tmean BMA QM 1979 2018
+srun python -u temprun.py tmean BMA QM 1979 2018
 
 
 
--- Plato_corrmerge3.sh
+++ Plato_corrmerge3.sh
@@ -11,7 +11,7 @@
 module load python/3.7.4
 
 # run the application
-srun python -u tempruh.py trange BMA QM 1979 2018
+srun python -u temprun.py trange BMA QM 1979 2018
 
 
 
