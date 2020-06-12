53521cd8c13ddade8e35d336d3002d99f1a58ffb
tgq14 <15201514761@163.com>
2020-06-12 17:00:40 -0600

    update

--- Plato_19.sh
+++ Plato_19.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=pop19
-#SBATCH --time=0-8:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --mem=10G
 module load python/3.7.4
 srun python -u reanalysis_pop.py 5401 5701
--- Plato_6.sh
+++ Plato_6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=pop6
-#SBATCH --time=0-8:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --mem=10G
 module load python/3.7.4
 srun python -u reanalysis_pop.py 1501 1801
