c483ae66986400b65b53739796a0263ccd809da7
tgq14 <15201514761@163.com>
2020-07-14 10:55:17 -0600

    new Clen

---
+++ Clen/generate_scripts.m
@@ -0,0 +1,17 @@
+% generate scripts for batch running
+% clc;clear
+vars={'prcp','tmean','trange'};
+
+for i=1:3
+    for j=1:12
+        outfile=['Plato_',varvv,num2str(i),'.sh'];
+        fidout=fopen(outfile,'w');
+        fprintf(fidout,'#!/bin/bash\n');
+        fprintf(fidout,['#SBATCH --job-name=clen\n']);
+        fprintf(fidout,'#SBATCH --time=0-5:00:00\n');
+        fprintf(fidout,'#SBATCH --mem=20G\n');
+        fprintf(fidout,'module load matlab/R2017b\n');
+        fprintf(fidout,['./Clen ',vars{i},' ',num2str(j)]);
+        fclose(fidout);
+    end
+end
--- temp.py
+++ temp.py
@@ -1,5 +1,7 @@
-d = io.loadmat(outfile_stn)
-data_stn = d['data_stn']
-LLE = d['LLE']
-date2 = d['date'][:]
-del d
+rankyv = np.zeros([dnum, gnum])
+for d in range(dnum):
+    for g in range(gnum):
+        dstn_dg = data_stn[d, g]
+        if dstn_dg > -100:
+            dens_dg = data_ens[d, g, :]
+            rankyv[d, g] = np.sum(dens_dg < dstn_dg)
